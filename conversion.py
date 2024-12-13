9fc2640702499dbe90b80dd1a24369e2183cd86a
sayfiddinruziyev <sayfiddinruziyev07@gmail.com>
2024-12-13 09:55:55 +0500

    uchinchisi

--- conversion.py
+++ conversion.py
@@ -1,12 +1,13 @@
-def to_octal(decimal_number):
-    octal = ""
+def to_hexadecimal(decimal_number):
+    hex_digits = "0123456789ABCDEF"
+    hexadecimal = ""
     while decimal_number > 0:
-        remainder = decimal_number % 8
-        octal = str(remainder) + octal
-        decimal_number //= 8
-    return octal
+        remainder = decimal_number % 16
+        hexadecimal = hex_digits[remainder] + hexadecimal
+        decimal_number //= 16
+    return hexadecimal
 
 # Misol
 decimal_number = 345
-octal_result = to_octal(decimal_number)
-print(f"{decimal_number} (10-lik) => {octal_result} (8-lik)")
+hexadecimal_result = to_hexadecimal(decimal_number)
+print(f"{decimal_number} (10-lik) => {hexadecimal_result} (16-lik)")
