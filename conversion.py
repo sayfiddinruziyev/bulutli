c59d9808064ac8655a70908f6383bb0339dede59
sayfiddinruziyev <sayfiddinruziyev07@gmail.com>
2024-12-13 09:55:05 +0500

    ikkinchisi

--- conversion.py
+++ conversion.py
@@ -1,14 +1,12 @@
-def to_binary(decimal_number):
-    binary = ""
+def to_octal(decimal_number):
+    octal = ""
     while decimal_number > 0:
-        remainder = decimal_number % 2
-        binary = str(remainder) + binary
-        decimal_number //= 2
-    return binary
+        remainder = decimal_number % 8
+        octal = str(remainder) + octal
+        decimal_number //= 8
+    return octal
 
-
-
-
-decimal_number = 45
-binary_result = to_binary(decimal_number)
-print(f"{decimal_number} (10-lik) => {binary_result} (2-lik)")
+# Misol
+decimal_number = 345
+octal_result = to_octal(decimal_number)
+print(f"{decimal_number} (10-lik) => {octal_result} (8-lik)")
